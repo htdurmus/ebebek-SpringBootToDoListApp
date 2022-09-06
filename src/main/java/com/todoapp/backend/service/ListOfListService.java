74a1d2fa885c57ba8f228e15995fcfc26f4392d7
Talha Durmus <kule_galata@hotmail.com>
2022-09-06 11:03:22 +0300

    Liste isimleri gösterme eklendi

--- src/main/java/com/todoapp/backend/service/ListOfListService.java
+++
@@ -1,9 +0,0 @@
-package com.todoapp.backend.service;
-
-import com.todoapp.backend.dto.ListOfListViewDTO;
-
-public interface ListOfListService {
-
-    ListOfListViewDTO getListOfListByName(String listName);
-
-}
